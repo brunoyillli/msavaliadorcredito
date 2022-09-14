ce6f7d6caacb6e95a55181d954ec71b40d71ab17
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-14 10:38:24 -0300

    servico de avaliacao de credito

--- src/main/java/io/github/brunoyillli/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ src/main/java/io/github/brunoyillli/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -3,12 +3,16 @@ package io.github.brunoyillli.msavaliadorcredito.application;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.brunoyillli.msavaliadorcredito.application.exceptions.DadosClienteNotFoundException;
 import io.github.brunoyillli.msavaliadorcredito.application.exceptions.ErroComunicacaoMicroservicesException;
+import io.github.brunoyillli.msavaliadorcredito.domain.model.DadosAvaliacao;
+import io.github.brunoyillli.msavaliadorcredito.domain.model.RetornoAvaliacaoCliente;
 import io.github.brunoyillli.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +33,19 @@ public class AvaliadorCreditoController {
 		try {
 			SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
 			return ResponseEntity.ok(situacaoCliente);
+		} catch (DadosClienteNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		} catch (ErroComunicacaoMicroservicesException e) {
+			return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity realizarAvalicao(@RequestBody DadosAvaliacao dados) {
+		try {
+			RetornoAvaliacaoCliente realizarAvaliacaoCliente = avaliadorCreditoService.realizarAvaliacao(dados.getCpf(),
+					dados.getRenda());
+			return ResponseEntity.ok(realizarAvaliacaoCliente);
 		} catch (DadosClienteNotFoundException e) {
 			return ResponseEntity.notFound().build();
 		} catch (ErroComunicacaoMicroservicesException e) {
--- src/main/java/io/github/brunoyillli/msavaliadorcredito/infra/clients/CartoesResourceClient.java
+++ src/main/java/io/github/brunoyillli/msavaliadorcredito/infra/clients/CartoesResourceClient.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.brunoyillli.msavaliadorcredito.domain.model.CartaoCliente;
+import io.github.brunoyillli.msavaliadorcredito.domain.model.Cartao;
 
 @FeignClient(value = "mscartoes", path = "/cartoes")
 public interface CartoesResourceClient {
 
 	@GetMapping(params = "cpf")
 	ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
+	
+	@GetMapping(params = "renda")
+	public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda);
+	
 }
